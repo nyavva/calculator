e7397e92676a46274c9349dd412f89704b8ba0b8
nyavva <anastasialukasinskaa@mail.com>
2026-05-18 21:14:22 +0300

    added keypad component

--- src/presentation/components/Keypad.tsx
+++ src/presentation/components/Keypad.tsx
@@ -14,15 +14,36 @@ export const Keypad = ({ctl}: Props) => {
         <View style={styles.grid}>
             <View style={styles.row}>
                 <Button label="AC" variant="function" onPress={ctl.clear} height={btnSize} />
-                <Button label="±" variant="function" onPress={ctl.toggleSign} height={btnSize} />
-                <Button label="%" variant="function" onPress={ctl.percent} height={btnSize} />
-                <Button label="÷" variant="function" onPress={() => ctl.inputOperator("/")} height={btnSize} />
+                <Button label="±" variant="operator" onPress={ctl.toggleSign} height={btnSize} />
+                <Button label="%" variant="operator" onPress={ctl.percent} height={btnSize} />
+                <Button label="÷" variant="operator" onPress={() => ctl.inputOperator("/")} height={btnSize} />
+            </View>
+            <View style = {styles.row}>
+                <Button label="7" onPress={() => ctl.inputDigit('7')} height={btnSize} />
+                <Button label="8" onPress={() => ctl.inputDigit('8')} height={btnSize} />
+                <Button label="9" onPress={() => ctl.inputDigit('9')} height={btnSize} />
+                <Button label="x" variant="operator" onPress={() => ctl.inputOperator("*")} height={btnSize} />
+            </View>
+            <View style={styles.row}>
+                <Button label="4" onPress={() => ctl.inputDigit('4')} height={btnSize} />
+                <Button label="5" onPress={() => ctl.inputDigit('5')} height={btnSize} />
+                <Button label="6" onPress={() => ctl.inputDigit('6')} height={btnSize} />
+                <Button label="-" variant="operator" onPress={() => ctl.inputOperator("-")} height={btnSize} />
+            </View>
+            <View style={styles.row}>
+                <Button label="1" onPress={() => ctl.inputDigit('1')} height={btnSize} />
+                <Button label="2" onPress={() => ctl.inputDigit('2')} height={btnSize} />
+                <Button label="3" onPress={() => ctl.inputDigit('3')} height={btnSize} />
+                <Button label="+" variant="operator" onPress={() => ctl.inputOperator("+")} height={btnSize} />
+                
+            </View>
+            <View style={styles.row}>
+                <Button label="0" flex={2} onPress={() => ctl.inputDigit('1')} height={btnSize} />
+                <Button label="." onPress={() => ctl.inputDecimal} height={btnSize} />
+                <Button label="=" variant="operator" onPress={() => ctl.evaluate} height={btnSize} />
             </View>
         </View>
-    )
-
-
-
+    );
 
 };
 
